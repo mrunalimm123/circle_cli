dbca8bd5123d3f105aec16f7d52ffccca34d1fe3
mrunalimm123 <104318948+mrunalimm123@users.noreply.github.com>
2022-04-24 23:59:50 +0530

    Update test2.py

--- .circleci/test2.py
+++ .circleci/test2.py
@@ -1,6 +1,6 @@
 def TestAdd():
-  assert Add(3,4)==7
-  print("Function works proper : ")
+    assert Add(3,4) == 7
+    print("Function works proper : ")
 
-if __name__="__main__":
-  TestAdd()
+if __name__=="__main__":
+    TestAdd()
